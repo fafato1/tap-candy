358242a553164e1c68fe7be4638aaeb6068e1951
fafato1 <fafato.889@gmail.com>
2019-10-14 15:36:14 -0300

    Update

--- js/trabalho.js
+++ js/trabalho.js
@@ -2,12 +2,36 @@ var candyHeart = 0;
 var tempo = 1000;
 var soldados = 0;
 var plusCandy = 1;
+var quantidadeParaCurar = 4;
 
 var jogador = {
-    forca: 0,
-    vida: 0,
+    forca : 0,
+    velAtq: 0,
+    vida  : 1,
     vidamaxima: 100,
 };
+var ajudante1 = {
+    forca : 0,
+    velAtq: 0,
+    vida  : 1,
+    vidamaxima: 100,
+};
+var ajudante2 = {
+    forca : 0,
+    velAtq: 0,
+    vida  : 1,
+    vidamaxima: 100,
+};
+
+var espada = {
+    dano  : 0,
+    velAtq: 0,
+}
+
+
+var inimigo = {
+    
+}
 
 function Update(){
     setTimeout(function(){MaisCW()}, tempo);
@@ -16,17 +40,20 @@ function Update(){
 
 function MaisCW(){
     candyHeart += plusCandy;
-    document.getElementById("CW").innerHTML = "Cora&#231&#245es doces: " + Math.trunc(candyHeart) + " | Cora&#231&#245es doces por segundo: " + plusCandy;
-    if((jogador.vida / jogador.vidamaxima) <= 1) {
+    document.getElementById("CW").innerHTML = "Cora&#231&#245es doces: " + Math.trunc(candyHeart) + " | Cora&#231&#245es doces por segundo: " + plusCandy + " | Cora&#231&#245es doces para curar: " + quantidadeParaCurar;
+    if(((jogador.vida / jogador.vidamaxima) <= 1) && (jogador.vida > 0)) {
         document.getElementById("barraVida").style.width = (jogador.vida / jogador.vidamaxima)*100 + "%";
         document.getElementById("barraVida").innerHTML = "Vida: " + Math.trunc((jogador.vida / jogador.vidamaxima)*100) + "% " + jogador.vida + "/" + jogador.vidamaxima;
-    jogador.vida++;
+        jogador.vida++;
+    }
+    else if(jogador.vida <= 0){
+
     }
     Update();
 }
 
 function Contratar(){
-    if((candyHeart > 20) && (plusCandy < 100)){
+    if((candyHeart > 20) && (plusCandy < 500)){
         candyHeart -=20
         soldados++;
         document.getElementById("soldados").innerHTML = "Soldados: " + soldados;
@@ -38,4 +65,24 @@ function Comer(){
     jogador.vidamaxima += Math.trunc(candyHeart / 20);
     jogador.forca += Math.trunc(candyHeart / 50);
     candyHeart = 0;
+}
+
+function Curar(){
+    if((jogador.vida / jogador.vidamaxima) < 1){
+        candyHeart -= quantidadeParaCurar;
+        jogador.vida = jogador.vidamaxima;
+        quantidadeParaCurar *= 4;
+    }
+}
+
+function Morrer(){
+    if(jogador.vida <= 0){
+
+    }
+    if(ajudante1.vida <= 0){
+
+    }
+    if(ajudante2.vida <= 0){
+        
+    }
 }
